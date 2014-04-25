379d53fe07d941f76f08536adf68394ef8164af5
Ian Chesal <ian.chesal@gmail.com>
2014-04-24 19:56:09 -0700

    One more tweak to the release flow.

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 git flow release start v$1
 sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" keybase/__init__.py
-rm -rf docs/generated
 python setup.py develop
 git commit keybase/__init__.py -m "Update to version v$1"
 git flow release finish v$1

cf3ab70599a316154300e49ea32724f4a295b33b
Ian Chesal <ian.chesal@gmail.com>
2014-04-24 19:42:18 -0700

    Update to version v0.1.1

--- keybase/__init__.py
+++ keybase/__init__.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 from . import keybase
 from .keybase import Keybase, KeybaseAdmin, KeybasePublicKey, gpg
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # do not set __package__ = "keybase", else we will end up with
 # keybase.<*allofthethings*>
